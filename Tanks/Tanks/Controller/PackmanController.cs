80817f32ed85e8a15a11c754aee1a7a2a2bcc520
Rezvanov Roman <rrrezzz@mail.ru>
2020-04-11 21:44:21 +0300

    add controller

---
+++ Tanks/Tanks/Controller/PackmanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tanks.Controller
+{
+    class PackmanController
+    {
+        private Timer timer = new Timer();
+        public int dt { get; }
+
+        PackmanController()
+        {
+            dt = timer.Interval;
+        }
+
+    }
+}
