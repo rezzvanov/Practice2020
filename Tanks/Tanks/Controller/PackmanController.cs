c1ea342937fa603c5e72cd51ea04c1348cc174be
Rezvanov Roman <rrrezzz@mail.ru>
2020-04-12 20:40:00 +0300

    modify controller

--- Tanks/Tanks/Controller/PackmanController.cs
+++ Tanks/Tanks/Controller/PackmanController.cs
@@ -3,19 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tanks.Model;
+using Tanks.View;
 using System.Windows.Forms;
 
 namespace Tanks.Controller
 {
     class PackmanController
     {
-        private Timer timer = new Timer();
-        public int dt { get; }
+        GameField gameField;
+        GameFieldView gameFieldView;
 
-        PackmanController()
+        public PackmanController(GameField gameField, GameFieldView gameFieldView)
         {
-            dt = timer.Interval;
+            this.gameField = gameField;
+            this.gameFieldView = gameFieldView;
+
+            gameField.timer.Start();
         }
 
+        private void handleInput(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+
+                    break;
+                case Keys.Left:
+                case Keys.A:
+                    gameField.Kolobok.Direction = Direction.Left;
+                    break;
+                case Keys.Up:
+                case Keys.W:
+                    gameField.Kolobok.Direction = Direction.Up;
+                    break;
+                case Keys.Right:
+                case Keys.D:
+                    gameField.Kolobok.Direction = Direction.Right;
+                    break;
+                case Keys.Down:
+                case Keys.S:
+                    gameField.Kolobok.Direction = Direction.Down;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
