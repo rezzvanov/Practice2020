1cceb80c072f79ffee5a1b3088bb84f35e4805a2
Rezvanov Roman <rrrezzz@mail.ru>
2020-04-15 22:46:47 +0300

    modify controller

--- Tanks/Tanks/Controller/PackmanController.cs
+++ Tanks/Tanks/Controller/PackmanController.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Tanks.Model;
 using Tanks.View;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Tanks.Controller
 {
@@ -13,14 +10,16 @@ namespace Tanks.Controller
     {
         GameField gameField;
         GameFieldView gameFieldView;
+        DateTime lastFire;
 
         public PackmanController(GameField gameField, GameFieldView gameFieldView)
         {
             this.gameField = gameField;
             this.gameFieldView = gameFieldView;
 
-            gameField.timer.Start();
-
+            gameField.ScoreUpdate += UpdateScore;
+            gameField.End += GameOver;
+            gameField.Win += WonGame;
             gameFieldView.MainForm.KeyDown += handleInput;
         }
 
@@ -29,7 +28,11 @@ namespace Tanks.Controller
             switch (e.KeyCode)
             {
                 case Keys.Space:
-                    gameField.Shot(gameField.Kolobok);
+                    if ((DateTime.Now - lastFire).TotalMilliseconds > 800)
+                    {
+                        gameField.Shoot(gameField.Kolobok);
+                        lastFire = DateTime.Now;
+                    }
                     break;
                 case Keys.Left:
                 case Keys.A:
@@ -50,6 +53,29 @@ namespace Tanks.Controller
                 default:
                     break;
             }
+        }
+
+        private void GameOver(object sender, EventArgs e)
+        {
+            gameField.timer.Stop();
+            gameFieldView.MainForm.Result.Text = "GameOver";
+            gameFieldView.MainForm.Result.ForeColor = Color.Red;
+            gameFieldView.MainForm.Result.Visible = true;
+            gameFieldView.MainForm.Button.Enabled = true;
+        }
+
+        private void WonGame(object sender, EventArgs e)
+        {
+            gameField.timer.Stop();
+            gameFieldView.MainForm.Result.Text = "Win";
+            gameFieldView.MainForm.Result.ForeColor = Color.Green;
+            gameFieldView.MainForm.Result.Visible = true;
+            gameFieldView.MainForm.Button.Enabled = true;
+        }
+
+        private void UpdateScore(object sender, EventArgs e)
+        {
+            gameFieldView.MainForm.Score.Text = $"Score: {gameField.Score}";
         }
     }
 }
