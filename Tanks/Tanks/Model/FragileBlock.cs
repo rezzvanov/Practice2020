ead485b29e25929c200313ce3a27853a624c279a
Rezvanov Roman <rrrezzz@mail.ru>
2020-04-16 21:18:55 +0300

    modify model

---
+++ Tanks/Tanks/Model/FragileBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace Tanks.Model
+{
+    public class FragileBlock : GameObject
+    {
+        public FragileBlock(Point initialPos, Size squareSize)
+            : base(initialPos, squareSize)
+        {
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "FragileBlock";
+            }
+        }
+    }
+}
---
+++ Tanks/Tanks/Model/River.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace Tanks.Model
+{
+    public class River : GameObject
+    {
+        public River(Point initialPos, Size squareSize)
+            : base(initialPos, squareSize)
+        {
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "River";
+            }
+        }
+    }
+}
